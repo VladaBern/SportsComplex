3886e44a8a8d0c5bb44f91f01c890dcbeeec1b6c
VladaBern <vladabernadskaya@gmail.com>
2023-06-27 19:30:12 +0300

    Added exception for invalid data

---
+++ SportsComplex/Exceptions/InvalidDataException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SportsComplex.Exceptions
+{
+    public class InvalidDataException : Exception
+    {
+        public InvalidDataException(string message) : base(message) { }
+    }
+}
