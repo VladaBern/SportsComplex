a1e77a8c4842804f9fc2c08ba69e51da7669782a
VladaBern <vladabernadskaya@gmail.com>
2023-06-27 19:31:45 +0300

    Added object for BadRequest

---
+++ SportsComplex/Responses/BadRequestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SportsComplex.Responses
+{
+    public class BadRequestResponse
+    {
+        public string ErrorMessage { get; }
+
+        public BadRequestResponse(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                throw new ArgumentNullException(nameof(message));
+
+            ErrorMessage = message;
+        }
+    }
+}
